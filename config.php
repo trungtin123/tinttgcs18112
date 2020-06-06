44359889c93be7e158317deee2bdd7e89eb7efb5
unknown <tinttgcs18112@fpt.edu.vn>
2020-06-06 10:26:28 +0700

    2nd

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'ec2-184-73-209-230.compute-1.amazonaws.com');
-define('DB_USERNAME', 'mtycxjjccxmeee');
-define('DB_PASSWORD', 'f0dd25aaa62a3a7a83d3934915c681f6ddfac747814e5a3668945ea34017c6ce');
-define('DB_NAME', 'd35915da8p1v65');
+define('DB_SERVER', 'ec2-52-44-55-63.compute-1.amazonaws.com');
+define('DB_USERNAME', 'unxekudwxkqfpd');
+define('DB_PASSWORD', 'e30160393b8a8c84b3813837f607760e9c19a07c0a989932babd360c7818fcf2');
+define('DB_NAME', 'dahvicesgsvher');
  
 /* Attempt to connect to PostgreSQL database */
 $link = pg_connect("host=".DB_SERVER." dbname=". DB_NAME ." user=" . DB_USERNAME . " password=" .DB_PASSWORD. "")
